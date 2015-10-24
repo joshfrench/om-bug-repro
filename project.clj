6b54df17181ec5758cbd352068f06b5003f969f6
Josh French <josh@vitamin-j.com>
2015-10-24 15:48:59 -0700

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject om-tutorial "0.1.0-SNAPSHOT"
+  :description "Om Next Tutorial"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.145"]
+                 [org.omcljs/om "1.0.0-alpha8"]
+                 [figwheel-sidecar "0.4.0" :scope "provided"]])
